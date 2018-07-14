274ad89e0aac2bac0d9b4516a1aa3223f3eec31f
sbrumley <sbrumley@wireless-town.com>
2018-07-14 13:04:55 -0400

    intial commit

---
+++ container.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+source vars
+
+case "$1" in
+
+# Intialize
+'init')
+mkdir -p /home/mcafee/misp-db
+sudo docker run -it --rm -v /home/mcafee/misp-db:/var/lib/mysql ${LOCATION} /init-db
+;;
+
+# Stop and Remove
+'remove')
+echo "Stopping and Removing Container ${NAME}"
+sudo docker stop ${NAME};sudo docker rm ${NAME}
+;;
+
+# Build Container
+'build')
+echo "Bulding Container ${NAME}"
+sudo docker build \
+    --rm=true --force-rm=true \
+    --build-arg MYSQL_MISP_PASSWORD=ChangeThisDefaultPassworda9564ebc3289b7a14551baf8ad5ec60a \
+    --build-arg POSTFIX_RELAY_HOST=localhost \
+    --build-arg MISP_FQDN=misp \
+    --build-arg MISP_EMAIL=admin@localhost \
+    -t ${LOCATION} .
+;;
+
+# Run Container
+'run')
+echo "Run Container ${NAME}"
+sudo docker run -it -d \
+    --name ${NAME} \
+    --restart=unless-stopped \
+    -p 443:443 \
+    -p 80:80 \
+    -p 3306:3306 \
+    -v /home/mcafee/misp/config/certs/:/etc/ssl/private \
+    -v /home/mcafee/misp-db:/var/lib/mysql \
+   ${LOCATION}
+;;
+
+# Purge All Containers
+'purge-all')
+echo "Purging All Containers ${NAME}"
+sudo docker system prune -a
+;;
+
+# Purge Stopped Containers
+'purge')
+echo "Purge Stopped Containers ${NAME}"
+sudo docker system prune
+;;
+
+*)
+echo "Things you can do with this script:"
+echo "Set container name and location in vars file"
+echo "./container init - Initialize Database"
+echo "./container build - Build Container"
+echo "./container remove - Remove Container"
+echo "./container run - Run Container"
+echo "./container purge-all - Purge all containers and images"
+echo "./container purge - Purge stopped containers and images"
+;;
+
+esac
